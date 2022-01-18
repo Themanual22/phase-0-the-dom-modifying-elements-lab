582f5c2f9d76cee1804330f97305d711f3bd3c6c
Themanual22 <djamosry22@gmail.com>
2022-01-18 08:27:49 -0500

    commit tree 0b97fbdbd5d443c5a69ae96096cf764a5ec51fb8

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ newHeader.setAttribute("id","victory");
 newHeader.innerHTML = "YOUR-NAME is the champion";
 
 element.append('h1');
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement("li");
+  li.textContent = (i + 1).toString();
+  ul.append(li);
+}
+
+element.append(ul);
